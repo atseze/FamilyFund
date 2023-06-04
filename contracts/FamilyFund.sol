91c14fd1a68d4674fe76f38cbed5c8c3094576c3
atseze <atseze@gmail.com>
2023-06-04 19:21:34 +0330

    remains state variable changed to local variable

--- contracts/FamilyFund.sol
+++ contracts/FamilyFund.sol
@@ -1,7 +1,6 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.17;
 
-// This to test git commit
 contract FamilyFund {
     uint private share;
     uint8 private round;
@@ -9,12 +8,12 @@ contract FamilyFund {
     bool private isFundActive;
     address private owner;
     address payable[] private members;
-    address payable[] private remains;
     mapping(address => mapping(uint8 => bool)) private paidShares; //each user in each round
-    mapping(address => uint8) private winners; //map each address to round of win
+    mapping(address => uint8) public winners; //map each address to round of win
 
     constructor(uint8 _membersCount, uint _share) {
-        require(_membersCount>1, "Fund must at least 2 member!");
+        require(_membersCount > 1, "Fund must have at least 2 member!");
+        require(_membersCount < 250, "Too many members!");
         membersCount = _membersCount;
         share = _share;
         owner = msg.sender;
@@ -22,14 +21,13 @@ contract FamilyFund {
         round = 1;
     }
 
-    function addMember(address payable _newMember) public OwnerOnly {
-        require(isFundActive == true, "Fund is closed!");
+    function addMember(address payable _newMember) public OwnerOnly MustActive {
         require(findMember(_newMember) == false, "Already added!");
         require(members.length < membersCount, "No empty space!");
         members.push(_newMember);
     }
 
-    function payShare() payable public {
+    function payShare() public payable MustActive {
         require(findMember(msg.sender) == true, "Not a member!");
         require(
             paidShares[msg.sender][round - 1] == false,
@@ -39,30 +37,37 @@ contract FamilyFund {
         paidShares[msg.sender][round - 1] = true;
     }
 
-    function chooseWinner() public OwnerOnly {
-        require(isFundActive == true, "Fund is closed!");
+    function chooseWinner() public OwnerOnly MustActive {
         require(members.length == membersCount, "Some members not added yet!");
         require(isAllPaidInRound() == true, "All members must paid in round!");
 
-        delete remains;
+        uint8 j = 0;
+        uint8[] memory remains = new uint8[](members.length);
         for (uint8 i = 0; i < members.length; i++)
-            if (winners[members[i]] == 0)
-                remains.push(members[i]);
+            if (winners[members[i]] == 0) {
+                remains[j] = i;
+                j++;
+            }
 
-        if (remains.length == 1) {
+        if (j == 1) {
             // this is the last winner
             weHaveAWinner(remains[0]);
             isFundActive = false;
-        } else if (remains.length > 1) {
+        } else if (j > 1) {
             //we need more round
-            uint winnerIndex = chooseRandom(remains.length);
-            weHaveAWinner(members[winnerIndex]);
+            uint8 winnerIndex = chooseRandom(j - 1);
+            weHaveAWinner(remains[winnerIndex]);
             round++;
         } else revert("Something bad is happened!");
     }
 
     modifier OwnerOnly() {
         require(msg.sender == owner, "Sender not authorized");
+        _;
+    }
+
+    modifier MustActive() {
+        require(isFundActive == true, "Fund is closed!");
         _;
     }
 
@@ -81,12 +86,12 @@ contract FamilyFund {
         return true;
     }
 
-    function chooseRandom(uint _count) private view returns (uint) {
-        return uint((block.prevrandao + block.timestamp) % _count);
+    function chooseRandom(uint _count) private view returns (uint8) {
+        return uint8((block.prevrandao + block.timestamp) % _count);
     }
 
-    function weHaveAWinner(address payable _winner) private {
-        winners[_winner] = round;
-        payable(_winner).transfer(address(this).balance);
+    function weHaveAWinner(uint8 _index) private {
+        winners[members[_index]] = round;
+        members[_index].transfer(address(this).balance);
     }
 }
